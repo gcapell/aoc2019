a847b2341928dba8e0abf11adac5208d6c59c5ff
Gary Capell <gary.capell@gmail.com>
2019-12-01 20:55:59 +1100

    First question.

---
+++ aoc1/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() {
+	let f = File::open("input.txt").unwrap();
+	let b = BufReader::new(f);
+	
+	let sum:u64 = b.lines()
+		.map(|r| r.unwrap())
+		.map(|l| mapper(&l))
+		.sum();
+	println!("{}", sum);	
+}
+
+fn mapper(l: &str)->u64 {
+	let n = l.trim().parse::<u64>().unwrap();
+	fuel(n)
+}
+
+fn fuel(mass: u64)-> u64 {mass/3 - 2}
+
+#[test]
+fn test_fuel() {
+	let test_data = [ (12,2), (14,2), (1969,654), (100756,33583)];
+	
+	for (m,f) in test_data.iter() {
+		assert_eq!(fuel(*m),*f);
+	}
+}
