f3195ad4369dd53cad6229896b7e85effb15544a
Gary Capell <gary.capell@gmail.com>
2019-12-12 08:09:08 +1100

    10.1 working

--- 10/10.go
+++ 10/10.go
@@ -1,9 +1,11 @@
 package main
+
 import (
-	"log"
-	"io/ioutil"
-	"strings"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"sort"
+	"strings"
 )
 
 func main() {
@@ -11,146 +13,102 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	lines := strings.Fields(string(data))
-	fmt.Println(len(lines), len(lines[0]), lines)
-	fmt.Println(best(lines))
+	fmt.Println(best(string(data)))
 }
 
-func best(lines[]string) (point, int) {
+type point struct{ x, y int }
+type asteroid struct {
+	point
+	d int
+}
+
+func best(s string) (point, int) {
+	points := findAsteroids(s)
+	withDistance := makeAsteroids(points)
 	max := 0
-	var maxp point
-	
-	for k :=0; k< len(lines); k++ {
-		for j :=0; j< len(lines[0]); j++ {
-			if lines[k][j] != '#' {
-				continue
-			}
-			p := point{j,k}
-			d := detect(lines, point{j,k})
-			// fmt.Printf("detect %d,%d -> %d\n", j,k, d)
-			if d > max {
-				maxp = p
-				max = d
+	var maxA point
+	for _, p := range points {
+		d := count(withDistance, p)
+		if d > max {
+			max = d
+			maxA = p
+		}
+	}
+	return maxA, max
+}
+
+func makeAsteroids(points []point) []*asteroid {
+	withDistance := make([]*asteroid, len(points))
+	for j, p := range points {
+		withDistance[j] = &asteroid{p, 0}
+	}
+	return withDistance
+}
+
+func findAsteroids(s string) []point {
+	lines := strings.Fields(s)
+	var reply []point
+	for y := 0; y < len(lines); y++ {
+		for x := 0; x < len(lines[0]); x++ {
+			if lines[y][x] == '#' {
+				reply = append(reply, point{x, y})
 			}
 		}
 	}
-	return maxp, max
-}
-
-
-type point struct  {x,y int}
-
-type queue struct  {
-	inbound, outbound []point
-}
-
-func(q *queue) push(p point) {
-	q.inbound = append(q.inbound, p)
-}
-
-func (q *queue) empty() bool {
-	return len(q.inbound) + len(q.outbound) == 0
-}
-
-func (q * queue) pop() point {
-	if len(q.outbound) == 0 {
-		q.outbound = q.inbound
-		q.inbound = nil
-		reverse(q.outbound)
-	}
-	reply := q.outbound[len(q.outbound)-1]
-	q.outbound = q.outbound[:len(q.outbound)-1]
 	return reply
 }
 
-func reverse(r []point) {
-	for j,k := 0, len(r)-1; j<k; j,k = j+1, k-1 {
-		r[j],r[k] = r[k],r[j]
+func count(asteroids []*asteroid, o point) int {
+	for _, a := range asteroids {
+		a.d = d2(a.point, o)
 	}
-}
-
-func detect(lines[]string, o point) int {
-	obscured := make(map[point]bool)
-	seen := make(map[point]bool)
-	edge := queue{}
-	edge.push(o)
-	seen[o] = true
-	
-	var neighbours [4]point
-	
+	sort.Slice(asteroids, func(i, j int) bool { return asteroids[i].d < asteroids[j].d })
+	if asteroids[0].point != o {
+		panic("sort?")
+	}
 	count := 0
-	for !edge.empty() {
-		p := edge.pop()
-		listNeighbours(p, neighbours[:])
-		for _, q := range neighbours {
-			if !inBounds(lines, q) {
-				continue
-			}
-			if seen[q] {
-				continue
-			}
-			seen[q] = true
-			edge.push(q)
-			if lines[q.y][q.x] == '.' || obscured[q] {
-				continue
-			}
-			count++
-			obscure(lines, obscured, o, q)
+outer:
+	for n, a := range asteroids {
+		if n == 0 {
+			continue
 		}
+		for _, b := range asteroids[1:n] {
+			if inline(o, b.point, a.point) {
+				continue outer
+			}
+		}
+		count++
 	}
 	return count
 }
 
-func inBounds(lines[]string, p point) bool {
-	return p.y>=0 && p.y < len(lines) && p.x >=0 && p.x < len(lines[0])
+func d2(a, b point) int {
+	dx := a.x - b.x
+	dy := a.y - b.y
+	return dx*dx + dy*dy
 }
 
-func listNeighbours(p point, dst []point) {
-	dst[0] = point{p.x, p.y+1}
-	dst[1] = point{p.x, p.y-1}
-	dst[2] = point{p.x+1, p.y}
-	dst[3] = point{p.x-1, p.y}
+func inline(a, b, c point) bool {
+	dx1 := b.x - a.x
+	dx2 := c.x - b.x
+	if !sameSign(dx1, dx2) {
+		return false
+	}
+	dy1 := b.y - a.y
+	dy2 := c.y - b.y
+	if !sameSign(dy1, dy2) {
+		return false
+	}
+	return dy1*dx2 == dx1*dy2
 }
 
-func obscure(lines []string, obscure map[point]bool, o, p point) {
-	dx := p.x - o.x
-	dy := p.y - o.y
-	
-	g := gcd(abs(dx), abs(dy))
-	dx, dy = dx/g, dy/g
-	
-	q := p
-	if o==p {
-		log.Fatal("obscure", o, p)
+func sameSign(a, b int) bool {
+	switch {
+	case a < 0:
+		return b < 0
+	case a == 0:
+		return b == 0
+	default:
+		return b > 0
 	}
-	for {
-		q.x += dx
-		q.y += dy
-		if !inBounds(lines,q) {
-			return
-		}
-		if lines[q.y][q.x] == '#' {
-			obscure[q] = true
-		}
-	}
-}
-
-func abs(a int) int {
-	if a<0 {
-		return -a
-	}
-	return a
-}
-
-func gcd (a,b int) int {
-	if a==0 || b ==0 {
-		return 1
-	}
-	if b>a {
-		a,b = b,a
-	}
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
 }
--- 10/10_test.go
+++ 10/10_test.go
@@ -1,47 +1,54 @@
 package main
-import (
-	"testing"
-	"strings"
-)
+
+import "testing"
 
 type td struct {
 	grid string
-	p point
-	n int
+	p    point
+	n    int
 }
-var testdata = []td{
-	{`
+
+var board1 = `
 	.#..#
 	.....
 	#####
 	....#
-	...##`,
-	point{3,4},
-	8,
-	},
-	{
-	`......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####`,
-	point{5,8},
-	33,
-	},
+	...##`
+
+var board2 = `......#.#.
+		#..#.#....
+		..#######.
+		.#.#.###..
+		.#..#.....
+		..#....#.#
+		#..#....#.
+		.##.#..###
+		##...#..#.
+		.#....####`
+
+func TestCount(t *testing.T) {
+	testdata := []td{
+		{board1, point{1, 0}, 7},
+	}
+	for _, td := range testdata {
+		got := count(makeAsteroids(findAsteroids(td.grid)), td.p)
+		if got != td.n {
+			t.Errorf("count(%s) -> %d want %d",
+				td.grid, got, td.n)
+		}
+	}
 }
 
-func
-TestDetect(t *testing.T) {
-	for _, td := range testdata {
-		p, n := best(strings.Fields(td.grid))
-	if ! (p == td.p && n == td.n) {
-		t.Errorf("best(%s) -> %v,%v; want %v, %v", 
-			td.grid, p, n, td.p, td.n)
+func TestDetect(t *testing.T) {
+	testdata := []td{
+		{board1, point{3, 4}, 8},
+		{board2, point{5, 8}, 33},
 	}
+	for _, td := range testdata {
+		p, n := best(td.grid)
+		if !(p == td.p && n == td.n) {
+			t.Errorf("best(%s) -> %v,%v; want %v, %v",
+				td.grid, p, n, td.p, td.n)
+		}
 	}
 }
