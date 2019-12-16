870f4624ef92cbd55f3d94a7bd73ad1f3e71cd9b
Gary Capell <ook@google.com>
2019-12-16 14:45:20 +1100

    day 9 parts 1 and 2

--- aoc9/src/interpreter.rs
+++ aoc9/src/interpreter.rs
@@ -22,27 +22,30 @@ pub fn new(prog: &[Word]) -> MachineState {
     }
 }
 
+pub fn single_input(prog: &[Word], i: Word) -> VecDeque<Word> {
+    let mut m = new(prog);
+
+    let mut input = VecDeque::new();
+    let mut output = VecDeque::new();
+    input.push_back(i);
+    match m.run(&mut input, &mut output) {
+        Signal::NeedsInput => panic!("input"),
+        Signal::Exit => output,
+    }
+}
+
 pub enum Signal {
     NeedsInput,
     Exit,
 }
 
 impl MachineState {
-    pub fn set_prog(&mut self, prog: &[Word]) {
-        self.pc = 0;
-        self.rbase = 0;
-        self.halted = false;
-        self.mem = prog.to_vec();
-    }
-
     pub fn run(&mut self, i: &mut VecDeque<Word>, o: &mut VecDeque<Word>) -> Signal {
         assert!(!self.halted);
         assert_eq!(o.len(), 0);
 
         loop {
-            let pc = self.pc;
             let (op, modes) = self.next_instruction();
-            println!("{} op:{}, modes:{:?}", pc, op, modes);
             match op {
                 1 => {
                     let (s1, s2, d) = self.params3(modes, S, S, D);
@@ -55,10 +58,7 @@ impl MachineState {
                 3 => {
                     let d = self.params1(modes, D);
                     match i.pop_front() {
-                        Some(val) => {
-                            self.mem_set(d, val);
-                            //println!("mem[{}] = {}", d, val);
-                        }
+                        Some(val) => self.mem_set(d, val),
                         None => return Signal::NeedsInput,
                     }
                 }
@@ -86,12 +86,7 @@ impl MachineState {
                     let (a, b, d) = self.params3(modes, S, S, D);
                     self.mem_set(d, bool_to_int(a == b));
                 }
-                9 => {
-                    let before = self.rbase;
-                    let p = self.params1(modes, S);
-                    self.rbase += p;
-                    println!("rbase {} + {} -> {}", before, p, self.rbase);
-                }
+                9 => self.rbase += self.params1(modes, S),
                 99 => {
                     self.halted = true;
                     return Signal::Exit;
@@ -145,6 +140,7 @@ impl MachineState {
             (S, 1) => v,
             (S, 2) => self.mem_get(v + self.rbase),
             (D, 0) => v,
+            (D, 2) => v + self.rbase,
             (D, _) => panic!("mode {} for Destination", mode),
             (S, _) => panic!("unknown mode {} for Src", mode),
         }
